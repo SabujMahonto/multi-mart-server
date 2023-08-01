ed0af6a290621a57f8470a9ee31a6ae22cc51358
sabujmahonto <sabujmahonto@gmail.com>
2023-08-01 10:01:37 +0600

    login user controller done

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,9 +1,9 @@
 const { createToken } = require("../helpers/token.helpers");
-const User = require("../models/user.model");
+const userModel = require("../models/user.model");
 const createUser = async (req, res) => {
   try {
     const { name, email, password, image, address, occupations } = req.body;
-    const user = await User.signup(
+    const user = await userModel.signup(
       name,
       email,
       password,
@@ -18,7 +18,16 @@ const createUser = async (req, res) => {
   }
 };
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.login(email, password);
+    const token = createToken(user.id);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createUser,
